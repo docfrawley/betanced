462cd26d23de69a577224be7d7c60cacce6646b0
Matt Frawley <docfrawley@gmail.com>
2015-10-15 21:08:28 -0400

    worked on board admin

--- includes/boardadmin.php
+++ includes/boardadmin.php
@@ -38,6 +38,69 @@ class boardadmin {
 		}
 	}
 
+	function add_bmember_form(){
+		?> 
+		 <form action="ncedboard.php" method="POST">
+		 	<fieldset>
+		 			<legend>Add Possible Board Member</legend>
+		 			Enter NCED number <strong>OR</strong> Last Name<br/><br/>
+		 	<div class="row">
+		 		<div class="small-3 columns">
+		 			<input type="text" name="ncednumber" placeholder="NCED Number"/>
+		 		</div>
+		 		<div class="small-6 columns">
+		 			<input type="text" name="LastName" placeholder="Last Name"/>
+		 		</div>
+		 		<div class="small-3 columns">
+		 			<input type="text" name="title" placeholder="Title(e.g. M.Ed.)"/>
+		 		</div>
+		 	</div>
+		 	<div class="row">
+		 		<div class="small-12 columns">
+		 			<label>bio</label>
+        			<textarea cols="40" rows="5" name="bio">
+        			</textarea>
+        		</div>
+        	</div>
+			<div class="row">
+		 		<div class="small-12 columns">
+        			<input type="submit" value="Submit" class="button tiny radius"/>
+        		</div>
+        	</div>
+        </fieldset>
+        </form><?
+	}
+
+	function edit_position_form($position, $person){
+		$this->set_poss_board();
+		$title = convert_key($position);
+		?><form action="ncedboard.php" method="post">
+		<fieldset>
+		 	<legend>Change Board Member for <? echo $title; ?> </legend>
+			<div class="row">
+		 		<div class="small-12 columns"><?
+					$boardMember = new boardMember($person);
+						?><label><? echo $title.": "; ?>
+					 		<select name="<? echo $position; ?>">
+					 			<option selected="selected" value="<? echo $person; ?>"><? echo $boardMember->get_name(); ?></option>
+		        				<? foreach ($this->possible_board as $nvalue) {
+		        					$bMember = new boardMember($nvalue);
+		        					echo '<option value="'. $nvalue. '">'.$bMember->get_name().'</option>';
+		        				} ?>
+				 			</select>
+				 		</label>
+		 		</div>
+		 	</div>
+			<div class="row">
+		 		<div class="small-12 columns">
+        			<input type="submit" value="Submit" class="button tiny radius"/>
+        		</div>
+        	</div>
+        	</fieldset>
+        </form><?
+	}
+
+
 	function generate_boxes(){
 	    foreach ($this->the_board as $key => $value) {
 	    	if ($key != 'boardindex'){
@@ -56,45 +119,40 @@ class boardadmin {
 	
 	function change_form(){
 		$this->set_poss_board();
-		?><form action="ncedboard.php" method="post"><?
-		foreach ($this->the_board as $key => $value) {
-			if ($key != 'boardindex'){
-				?>
-				<div class="row">
-		 			<div class="small-12 columns">
-						 <?
+		?><form action="ncedboard.php" method="post">
+		<fieldset>
+		 	<legend>Click on Board Position to Change Member</legend>
+		 	<div class="row">
+		 		<div class="small-12 columns">
+				<?
+				foreach ($this->the_board as $key => $value) {
+					if ($key != 'boardindex'){
 						$title = convert_key($key);
 						$boardMember = new boardMember($value);
-						?><label><? echo $title.": "; ?>
-					 		<select name="<? echo $key; ?>">
-					 			<option selected="selected" value="<? echo $value; ?>"><? echo $boardMember->get_name(); ?></option>
-		        				<? foreach ($this->possible_board as $nvalue) {
-		        					$bMember = new boardMember($nvalue);
-		        					echo '<option value="'. $nvalue. '">'.$bMember->get_name().'</option>';
-		        				} ?>
-				 			</select>
-				 		</label>
-		 			</div>
-		 		</div>
-				<?
-			}
-		} ?>
-		<div class="row">
-		 			<div class="small-12 columns">
-        				<input type="submit" value="Submit" class="button tiny radius"/>
-        			</div>
-        		</div>
+						echo '<a href="?member='. $value.'&position='.$key.'">'.$title.'</a>: ';
+						echo $boardMember->get_name();
+						echo "<br/>";
+					}
+				}?>	
+				<br/>
+				</div>
+			</div>
+        </fieldset>
         </form><?
 	}
 
 	function members_form(){
 		$this->set_poss_board(); ?>
 		<div class="row">
-			<div class="small-12 columns"> <?
-				foreach ($this->possible_board as $nvalue) {
-				    $bMember = new boardMember($nvalue);
-				    echo '<a href="?member='. $nvalue.'&task=edit">'.$bMember->get_name().'</a><br/>';
-				} ?>
+			<div class="small-12 columns"> 
+				<fieldset>
+		 			<legend>Edit/Delete Board Members</legend>
+					<?
+					foreach ($this->possible_board as $nvalue) {
+					    $bMember = new boardMember($nvalue);
+					    echo '<a href="?member='. $nvalue.'&task=edit">'.$bMember->get_name().'</a><br/>';
+					} ?>
+				</fieldset>
 			</div>
 		</div><?
 	}
--- public/ncedboard.php
+++ public/ncedboard.php
@@ -6,10 +6,17 @@ if (isset($_SESSION['ncedadmin'])) {
 
 $the_board = new boardadmin(); ?>
 <div class="row"> 
-	<div class="medium-6 columns">
-		<? $the_board->change_form(); ?>
+	<div class="medium-7 columns">
+		<div class="row"> 
+			<div class="medium-12 columns">
+				<? $the_board->add_bmember_form(); ?>
+			</div>
+			<div class="medium-12 columns">
+				<? $the_board->change_form(); ?>
+			</div>
+		</div>
 	</div>
-	<div class="medium-6 columns">
+	<div class="medium-5 columns">
 		<? $the_board->members_form(); ?>
 	</div>
 </div> <?
