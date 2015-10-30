75852dc7892ad7b490fdb56e6cd8087d3f3942f0
Matt Frawley <docfrawley@gmail.com>
2015-10-30 15:24:01 -0400

    finished announce admin

--- includes/announce.php
+++ includes/announce.php
@@ -74,6 +74,9 @@ class all_announcements {
 	
 	function delete_announce($numentry){
 		global $database;
+		$announce = new announceobject($numentry);
+		$priority = $announce->get_priority();
+		$this->set_priorities(0, $priority);
 		$sql = "DELETE FROM announce ";
 	  	$sql .= "WHERE id=". $database->escape_value($numentry);
 	  	$sql .= " LIMIT 1";
@@ -83,16 +86,56 @@ class all_announcements {
 	
 	function update_announce($info){
 		global $database;
+		$id = $database->escape_value($info['id']);
+		$priority = $database->escape_value($info['priority']);
+		$announce = new announceobject($id);
+		$prev = $announce->get_priority();
+		if ($announce->get_priority() !=  $priority){
+			$this->set_priorities($priority, $prev); 
+		}
 		$sql = "UPDATE announce SET ";
 		$sql .= "title='". $database->escape_value($info['title']) ."', ";
 		$sql .= "announcement='". nl2br($database->escape_value($info['announcement'])) ."', ";
-		$sql .= "priority='". $database->escape_value($info['priority']) ."' ";
-		$sql .= "WHERE id='". $database->escape_value($info['id'])."'";
+		$sql .= "priority='". $priority ."' ";
+		$sql .= "WHERE id='". $id ."'";
 	  	$database->query($sql);
+	}
+
+	function set_priorities($num, $prev=0){
+		global $database;
+		$this->set_announcements();
+		if ($prev == 0){
+			$prev = $this->num_announce();
+		} elseif ($num == 0 ) {
+			$num = $this->num_announce();
+		}
+		if ($prev < $num){
+			$counter = $prev;
+			for ($x = $prev; $x < $num; $x++){
+				$info = new announceobject($this->an_array[$x]);
+				$sql = "UPDATE announce SET ";
+				$sql .= "priority='". $counter ."' ";
+				$sql .= "WHERE id='". $info->get_id()."'";
+		  		$database->query($sql);
+		  		$counter++;
+			}
+		} else {
+			$counter = $num;
+			for ($x = $num - 1; $x < $prev; $x++){
+				$counter++;
+				$info = new announceobject($this->an_array[$x]);
+				$sql = "UPDATE announce SET ";
+				$sql .= "priority='". $counter ."' ";
+				$sql .= "WHERE id='". $info->get_id()."'";
+		  		$database->query($sql);
+			}
+		}
+		
 	}
 	
 	function add_announce($info){
 		global $database;
+		$this->set_priorities($database->escape_value($info['priority']));
 		$sql = "INSERT INTO announce (";
 	  	$sql .= "title, announcement, priority";
 	  	$sql .= ") VALUES ('";
