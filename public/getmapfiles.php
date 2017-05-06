bf361d43d9e7e95391374ff7ecf302de0b858f57
Matt Frawley <docfrawley@gmail.com>
2017-05-06 13:50:49 -0400

    more maps

--- public/getmapfiles.php
+++ public/getmapfiles.php
@@ -1,5 +1,5 @@
 <?php require_once("../includes/initialize.php");
-
+      header('Access-Control-Allow-Origin: *'); 
 if ($_GET['task']=='markers'){
   $map_markers = new all_maps();
   $result = $map_markers->get_markers();
